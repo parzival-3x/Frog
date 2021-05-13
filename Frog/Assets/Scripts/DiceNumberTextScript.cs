14ad3084434c6486f29a3944059a0abd82f6611b
parzival-3x <bg193b@gmail.com>
2021-05-13 14:39:44 -0500

    added rudimentary score system

--- Frog/Assets/Scripts/DiceNumberTextScript.cs
+++ Frog/Assets/Scripts/DiceNumberTextScript.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class DiceNumberTextScript : MonoBehaviour {
 
-	Text text;
+	public TextMeshProUGUI Scoretext;
 	public static int diceNumber;
 
 	// Use this for initialization
 	void Start () {
-		text = GetComponent<Text> ();
+		Scoretext.text = "Your Score: ";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		text.text = diceNumber.ToString ();
+		
+		Scoretext.text ="Your Score: "+ diceNumber.ToString();
 	}
 }

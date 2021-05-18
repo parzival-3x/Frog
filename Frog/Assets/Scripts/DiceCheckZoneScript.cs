c2b9b8afc31a9f368fa9f5de9dabbe59f88cb13b
parzival-3x <bg193b@gmail.com>
2021-05-18 14:47:38 -0500

    trying to silence ontriggerstay

--- Frog/Assets/Scripts/DiceCheckZoneScript.cs
+++ Frog/Assets/Scripts/DiceCheckZoneScript.cs
@@ -36,5 +36,6 @@ public class DiceCheckZoneScript : MonoBehaviour {
 				break;
 			}
 		}
+		GetComponent<DiceCheckZoneScript>().enabled = false;
 	}
 }
--- Frog/Assets/Scripts/DiceNumberTextScript.cs
+++ Frog/Assets/Scripts/DiceNumberTextScript.cs
@@ -13,16 +13,25 @@ public class DiceNumberTextScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Scoretext.text = "Your Score: ";
+		array.Clear();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (diceNumber != 0)
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+			array.Clear();
+        }
+			if (diceNumber != 0)
         {
 			array.Add(diceNumber);
 			diceNumber = 0;
 			Debug.Log(array.Count);
 		}
+        if (array.Count > 4)
+        {
+			Debug.Log(array[0]+" "+ array[1] + " " + array[2] + " " + array[3] + " " + array[4] + " ");
+        }
 		//Scoretext.text ="Your Score: "+ diceNumber.ToString();
 	}
 	void DisplayScore()
